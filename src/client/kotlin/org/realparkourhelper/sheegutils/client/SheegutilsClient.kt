1940f0144617905e67ce0dabfe80a55aab358cfc
senezdev <dudnikoff98@gmail.com>
2025-08-09 13:40:38 +0300

    initial commit

---
+++ src/client/kotlin/org/realparkourhelper/sheegutils/client/SheegutilsClient.kt
@@ -0,0 +1,9 @@
+package org.realparkourhelper.sheegutils.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class SheegutilsClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
---
+++ src/main/kotlin/org/realparkourhelper/sheegutils/Sheegutils.kt
@@ -0,0 +1,9 @@
+package org.realparkourhelper.sheegutils
+
+import net.fabricmc.api.ModInitializer
+
+class Sheegutils : ModInitializer {
+
+    override fun onInitialize() {
+    }
+}
